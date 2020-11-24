da9cd6f6f3a3d6a9514b8a1db7d4f2e937219520
Xu Hongyi <hongyi.xu@gmail.com>
2020-11-24 08:37:41 +0800

    change youtube-dl download directory

--- apps/youtube-dl/bin/youtube-dl.sh
+++ apps/youtube-dl/bin/youtube-dl.sh
@@ -3,7 +3,7 @@ docker run --rm \
            --tty \
            --env DISPLAY=host.docker.internal:0.0 \
            --env LIBGL_ALWAYS_INDIRECT=0 \
-           --volume $HOME/Workspace/pyapps/apps/youtube-dl:/home/xuhy/app \
+           --volume /mnt/d/Temporary:/home/xuhy/app \
            --net host \
            --user xuhy \
            --workdir /home/xuhy/app \
